a5a2c43f7ee3197a76d5ca0c74360e0dfcdba4de
Your own username <Your own email>
2025-05-04 12:09:59 +0800

    Complete Part A and Part B - Express API practical

---
+++ Practical01/Practical01/PartB_Homework/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Welcome to Homework API");
+});
+
+app.get('/intro', (req, res) => {
+  res.send("Hi, I'm Gary, a student learning to build APIs with Express!");
+});
+
+app.get('/name', (req, res) => {
+  res.send("Gary Chong");
+});
+
+app.get('/hobbies', (req, res) => {
+  res.json(["coding", "gaming", "music"]);
+});
+
+app.get('/food', (req, res) => {
+  res.send("Ramen, Fried Chicken, Bubble Tea");
+});
+
+app.get('/student', (req, res) => {
+  res.json({
+    name: "Gary",
+    hobbies: ["coding", "gaming", "music"],
+    intro: "Hi, I'm Gary, a Year 2 student passionate about building APIs!"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
